a933c85146a90e523cb55133c9c763848b42c3b3
multiverse22 <momizi527@gmail.com>
2024-09-11 16:28:56 +0900

    application properties 수정 및 데이터베이스 연결 및 환경변수 세팅으로 서버가 뜨는지 확인했습니다. AOP를 활용해 @Admin 어노테이션이 붙은 컨트롤러에 접근한사람이 누구인지, 언제인지,어떤 URI에서 접근했는지 로깅하는 기능을 구현했습니다. URI를 구현할때는 HttpServletRequest를 활용해 Http 요청에 관한 정보를 찾아 활용하였습니다.

--- src/main/java/org/example/expert/config/AspectAdmin.java
+++ src/main/java/org/example/expert/config/AspectAdmin.java
@@ -1,24 +1,28 @@
 package org.example.expert.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Component
 @Aspect
 @Slf4j
 public class AspectAdmin {
-    LocalDate now = LocalDate.now();
+    LocalDateTime now = LocalDateTime.now();
 
     @Pointcut("@annotation(org.example.expert.annotation.Admin)")
     private void adminDeleteComment() {}
-
 
     @Around("adminDeleteComment()&&args(authUser,..)")
     public Object adviceAdminDeleteComment(ProceedingJoinPoint joinPoint,AuthUser authUser) throws Throwable {
@@ -30,11 +34,12 @@ public class AspectAdmin {
             Object result = joinPoint.proceed();
             return result;
         } finally {
-            log.info("요청 사용자 id={}", authUser.getId());
-            log.info("현재 시각 {}",now);
-            log.info("API 요청 URL:/admin/comments/{commentId}");
+            log.info("요청 사용자 id={}", authUser.getId()); //- 요청한 사용자의 ID
+            log.info("현재 시각 {}",now); //- API 요청 시각
+            HttpServletRequest request =
+                    ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+            log.info(request.getMethod()+" : "+request.getServletPath()); //- API 요청 URL 와 HTTP Method
         }
     }
-
 
 }
